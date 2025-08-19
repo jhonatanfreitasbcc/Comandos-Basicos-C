f215fff1d725fd3f972d778c32ee64c404507011
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2025-08-19 17:17:51 -0300

    Imprimir uma String Várias Vezes com Laço FOR.c

---
+++ Imprimir uma String várias vezes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    
+    int i;
+    char nome[10] = "jhonatan";
+    
+    for (i = 0; i < 20; i++){
+        printf("%s\n", nome);
+    }
+    
+    return 0;
+    
+};
