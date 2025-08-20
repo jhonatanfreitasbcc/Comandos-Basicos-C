1393791e8f8716194cef960b77e6902008ba8cf0
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2025-08-20 14:58:35 -0300

    Função que Recebe 2 Números e Calcula a Multiplicação.c

---
+++ Função que Recebe 2 Números e Calcula a Multiplicação.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int somaNumeros(int numeroA, int numeroB){
+    return numeroA * numeroB;
+}
+
+int main(){
+    int numeroA, numeroB, resultado;
+    printf("Digite um numeroA:");
+    scanf("%d", &numeroA);
+    
+    printf("Digite um numeroB:");
+    scanf("%d", &numeroB);
+    
+    resultado = somaNumeros(numeroA, numeroB);
+    
+    printf("A Multiplicação é: %d\n", resultado);
+    
+    return 0;
+}
