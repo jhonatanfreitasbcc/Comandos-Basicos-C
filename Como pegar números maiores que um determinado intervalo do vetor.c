4b8e7f67c41b53627c4440350face52c4a5ccc4f
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2025-10-10 16:26:40 -0300

    Como pegar números maiores que um determinado intervalo do vetor.c

---
+++ Como pegar números maiores que um determinado intervalo do vetor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    
+    int vetor[] = {1, 3, 7, 8, 9};
+    int tamanho = 5;
+    int i;
+
+    printf("os numeros maiores que 5 são: ");
+
+
+    for (i = 0; i < tamanho; i++){
+        if (vetor[i] > 5){
+            printf("%d ", vetor[i]);
+        }
+    }
+    
+    
+    return 0;
+}
+ 
